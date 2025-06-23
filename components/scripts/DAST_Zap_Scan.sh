739b7bdada7a4b1c3fee089f69dd1bff1ebd1ec8
chaehj02 <53753819+chaehj02@users.noreply.github.com>
2025-06-24 01:15:30 +0900

    Update DAST_Zap_Scan.sh

--- components/scripts/DAST_Zap_Scan.sh
+++ components/scripts/DAST_Zap_Scan.sh
@@ -8,7 +8,7 @@ BUILD_NUMBER="${BUILD_NUMBER:-1}"
 REGION="${REGION:-ap-northeast-2}"
 ECR_REPO="${ECR_REPO}"
 ZAP_SCRIPT="${ZAP_SCRIPT:-zap_webgoat.sh}"
-S3_BUCKET="${S3_BUCKET:-my-bucket}"
+SS3_BUCKET="${S3_BUCKET:-testdast}"
 
 # 동적 변수 설정
 containerName="${CONTAINER_NAME}-${BUILD_NUMBER}"
